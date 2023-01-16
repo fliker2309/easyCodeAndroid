7a7159a97cc192aac569145d3968d1f384bf04a1
Evgenii <evgeniikapchik@gmail.com>
2023-01-16 22:48:46 +0300

    add glide dependency

--- app/src/main/java/com/example/emptyproject/MainActivity.kt
+++ app/src/main/java/com/example/emptyproject/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.example.emptyproject
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
+
+private const val URL =
+    "htt[s://zavistnik.com/wp-content/uploads/2020/03/Android-kursy-zastavka.jpg"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val image = findViewById<ImageView>(R.id.main_IV)
+        val netImage = NetImage(
+            URL,
+            object : ImageCallback {
+                override fun success(bitmap: Bitmap) = runOnUiThread{
+                    image.setImageBitmap(bitmap)
+                }
+
+                override fun failed() {
+                    Snackbar.make(image, "failed", Snackbar.LENGTH_SHORT).show()
+                }
+            }
+        )
+        netImage.start()
     }
 }
